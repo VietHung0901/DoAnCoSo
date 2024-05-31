833f94083c32e41560d2a225093e78195df8617e
quocdeptrai <nguyenvietq306@gmail.com>
2024-05-31 21:17:07 +0700

    Deatial index quydinh-noidung user, admin. Detail Cuocthi user-admin

--- DoAnCoSo/DoAnCoSo/obj/Debug/net8.0/DoAnCoSo.AssemblyInfo.cs
+++ DoAnCoSo/DoAnCoSo/obj/Debug/net8.0/DoAnCoSo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnCoSo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59249c1acd7182a54d9c4229880638ea0936cf60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+131d7630183d7aa89187ee8972954ded069ee039")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnCoSo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnCoSo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
